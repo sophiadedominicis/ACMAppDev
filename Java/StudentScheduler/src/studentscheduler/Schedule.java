168aa4ccdc12d4265560b67b3b77ec7226cfee57
Michael Dombrowski <Michael@mikedombrowski.com>
2015-10-14 21:35:32 -0400

    schedule

--- Java/StudentScheduler/src/studentscheduler/Schedule.java
+++ Java/StudentScheduler/src/studentscheduler/Schedule.java
@@ -9,6 +9,58 @@ package studentscheduler;
  *
  * @author Mike
  */
+import java.util.ArrayList;
 public class Schedule {
+    private ArrayList<Section> listOfSections;
+    private int numberOfClasses;
+    private double numberOfUnits;
+    private int earliestTime;
+    private int latestTime;
+    private boolean fridayFree;
     
+    public Schedule(){
+    }
+    
+    public void addSection(Section sec){
+        listOfSection.add(sec);
+        numberOfClasses++;
+        numberOfUnits += sec.getNumUnits();
+        if(sec.getEarliestTime() < earliestTime){
+            earliestTime = sec.getEarliestTime();
+        }
+        if(sec.getLatestTime() > latestTime){
+            latestTime = sec.getLatestTime();
+        }
+        if(sec.meetsFriday()){
+            fridayFree = false;
+        }
+    }
+    
+    public ArrayList<Section> getSchedule(){
+        return listOfSections;
+    }
+    
+    public ArrayList<Course> getCourses(){
+        ArrayList<Course> courses = new ArrayList<>();
+        for(Section c : listOfSections){
+            courses.add((Course) Section);
+        }
+        return courses;
+    }
+    
+    public int getNumClasses(){
+        return numberOfClasses;
+    }
+    
+    public int getEarliestTime(){
+        return earliestTime;
+    }
+    
+    public int getlatestTime(){
+        return latestTime;
+    }
+    
+    public boolean fridayFree(){
+        return fridayFree;
+    }
 }
